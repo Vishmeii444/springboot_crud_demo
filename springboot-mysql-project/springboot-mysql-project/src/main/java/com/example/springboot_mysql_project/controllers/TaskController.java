9dc903481fe119b3eb41ad680ce759ce9aaa6f5e
Vishmi <vishmihgangodawila@gmail.com>
2026-05-11 10:21:27 +0530

    Add OpenAPI annotations to TaskController
    Annotate TaskController with OpenAPI/Swagger metadata to improve API documentation. Adds @Tag on the controller, @Operation summaries for GET/POST/PUT/DELETE endpoints, and an @ApiResponse for the POST endpoint (201 created). Also adds the necessary imports for io.swagger.v3.oas.annotations.

--- springboot-mysql-project/springboot-mysql-project/src/main/java/com/example/springboot_mysql_project/controllers/TaskController.java
+++ springboot-mysql-project/springboot-mysql-project/src/main/java/com/example/springboot_mysql_project/controllers/TaskController.java
@@ -2,6 +2,8 @@ package com.example.springboot_mysql_project.controllers;
 
 import com.example.springboot_mysql_project.entities.Task;
 import com.example.springboot_mysql_project.services.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +14,13 @@ import java.util.List;
 // All the endpoints in this starts with /tasks
 
 @RequestMapping("/tasks")
+@Tag(name = "Task Management", description = "Operations related to managing tasks in the CRUD application")
 public class TaskController {
 
     @Autowired
     private TaskService taskService;
 
+    @Operation(summary = "Get all tasks", description = "Fetches all the tasks within the database")
     // Get all the tasks available
     @GetMapping
     // this will basically carry out SELECT * FROM task
@@ -24,6 +28,8 @@ public class TaskController {
         return taskService.getAllTasks();
     }
 
+    @Operation(summary = "Create a new task")
+    @ApiResponse(responseCode = "201", description = "Task successfully created")
     // Add a new task
     @PostMapping
     // this will carry out INSERT INTO task
@@ -31,6 +37,7 @@ public class TaskController {
         return taskService.createTask(task);
     }
 
+    @Operation(summary = "Update an existing task")
     // Modify an existing task
     @PutMapping("/{id}")
     // this will carry out SELECT * FROM task WHERE id = smth
@@ -38,6 +45,7 @@ public class TaskController {
         return taskService.updateTask(id, updatedTask);
     }
 
+    @Operation(summary = "Delete an existing task")
     // Remove a task
     @DeleteMapping("/{id}")
     // this will carry out DELETE FROM task WHERE id = smth
